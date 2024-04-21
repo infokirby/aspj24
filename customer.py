03f23bdc4a1050a0d4fc19da3d897f195a570fee
infokirby <kohkady23@gmail.com>
2024-04-21 23:47:00 +0800

    first commit

---
+++ customer.py
@@ -0,0 +1,158 @@
+#import User
+
+class Customer():
+    def __init__(self, phoneNumber):
+        self.__phoneNumber = str(phoneNumber)
+
+    # accessor methods
+    def get_customer_id(self):
+        return self.__customer_id
+    
+    @property
+    def get_datetime(self):
+        return self.__datetime
+
+    @property
+    def get_stallName(self):
+        return self.__stallName
+    
+    @property
+    def get_orderID(self):
+        return self.__orderID
+    
+    @property
+    def get_item(self):
+        return self.__item
+    
+    @property
+    def get_itemQuantity(self):
+        return self.__itemQuantity
+    
+    @property
+    def get_ingredient(self):
+        return self.__ingredient
+    
+    @property
+    def get_ingredientQuantity(self):
+        return self.__ingredientQuantity
+    
+    @property
+    def get_price(self):
+        return self.__price
+    
+    @property
+    def get_total(self):
+        return self.__total
+    
+    @property
+    def get_remarks(self):
+        return self.__remarks
+    
+    @property
+    def get_status(self):
+        return self.__status
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_id(self):
+        return str(self.__phoneNumber)
+    
+    def get_password(self):
+        return self.__password
+    
+    def get_gender(self):
+        return self.__gender
+    
+    def get_membership(self):
+        return self.__membership
+    
+    def get_securityQuestion(self):
+        return self.__securityQuestion
+    
+    def get_securityAnswer(self):
+        return self.__securityAnswer
+
+    # mutator methods
+    def set_customer_id(self, customer_id):
+        self.__customer_id = customer_id
+
+    # def set_food(self, food):
+    #     self.__food = food
+
+    # def set_quantity(self, quantity):
+    #     self.__quantity = quantity
+
+    # def set_remark(self, remark):
+    #     self.__remark = remark
+
+    # def set_order_time(self,order_time):
+    #     self.__order_time = order_time
+        
+    def set_datetime(self, datetime):
+        self.__datetime = datetime
+        
+    def set_stallName(self, stallName):
+        self.__stallName = stallName
+
+    def set_orderID(self, orderID):
+        self.__orderID = orderID
+
+    def set_item(self, item):
+        self.__item = item
+
+    def set_itemQuantity(self, itemQuantity):
+        self.__itemQuantity = itemQuantity
+
+    def set_ingredient(self, ingredient):
+        self.__ingredient = ingredient
+
+    def set_ingredientQuantity(self, ingredientQuantity):
+        self.__ingredientQuantity = ingredientQuantity
+
+    def set_price(self, price):
+        self.__price = price
+
+    def set_total(self, total):
+        self.__total = total
+
+    def set_remarks(self, remarks):
+        self.__remarks = remarks
+
+    def set_status(self, status):
+        self.__status = status
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_id(self, phoneNumber):
+        self.__id = phoneNumber
+
+    def set_password(self, password):
+        self.__password = password
+
+    def set_gender(self, gender):
+        self.__gender = gender
+
+    def set_membership(self, membership):
+        self.__membership = membership
+
+    def set_securityQuestion(self, securityQuestion):
+        self.__securityQuestion = securityQuestion
+
+    def set_securityAnswer(self, securityAnswer):
+        self.__securityAnswer = securityAnswer    
+
+#Login requirements
+    def is_active(self):
+        return True
+    
+    def is_anonymous(self):
+        return False
+    
+    def is_authenticated(self):
+        return True
+    
+#__str__ function
+    def __str__(self):
+        return f"User {self.get_name()} with phone number {self.get_id()}"
---
+++ menu.py
@@ -0,0 +1,107 @@
+menu = {
+
+    "Drinks": {
+        "Kopi": 1,
+        "Teh": 1,
+        "Iced Lemon Tea": 1.2,
+        "Calamansi": 1.2,
+        "Iced Milo": 1,
+    },
+
+    "Vegetarian": {
+        "Bee Hoon": 2,
+        "Spring Rolls": 3,
+        "Fried Rice": 2.5,
+        "Rice": 1,
+        "Stir Fried Vegetables": 0.5,
+    },
+    "Muslim": {
+        "Rice" : 1,
+        "Curry Chicken": 0.7,
+        "Fried Egg": 0.5,
+        "Chicken Wing": 0.7
+    },
+    "Indian": {
+        "Dosa": 2.5,
+        "Plain Prata": 1.5,
+        "Egg Prata": 2,
+        "Murtabak": 3,
+    },
+    "Chicken Rice": {
+        "Roasted Chicken Rice": 3,
+        "Steamed Chicken Rice": 3,
+        "Char Siew Rice": 3.5,
+        "Roasted Pork Rice": 3.5,
+    },
+    "Pizza": {
+        "Cheese Pizza": 4,
+        "Mac & Cheese": 3.8,
+        "Chicken Burrito": 3.8,
+        "Baked Pasta": 3.8,
+    },
+    "Japanese": {
+        "Salmon Don": 3.8,
+        "Kaarage": 2.5,
+        "Curry Rice": 3.5,
+    },
+    "Ban Mian": {
+        "Ban Mian": 3,
+        "Dumplings": 2.5,
+        "Tom Yum U Mian": 3,
+        "Fish Slice Soup with Bee Hoon": 3.2
+    },
+    "Curry Rice": {
+        "Chicken Cutlet Curry Rice": 3.5,
+        "Curry Rice": 3,
+        "Pork Cutlet Curry Rice": 3.5,
+        "Fried Ebi Curry Rice": 3.5
+    },
+    "Yong Tau Foo": {
+        "Wongbok": 0.5,
+        "Tau Pok": 0.5,
+        "Tau Kwa": 0.5,
+        "Fishball": 0.5,
+        "Fishcake": 0.5,
+        "Enoki Mushroom": 0.5,
+        "Luncheon Meat": 0.7,
+        "Koka Noodles": 0.5
+    },
+    "Mala": {
+        "Wongbok": 0.7,
+        "Tau Pok": 0.7,
+        "Tau Kwa": 0.7,
+        "Fishball": 0.7,
+        "Fishcake": 0.7,
+        "Enoki Mushroom": 0.7,
+        "Luncheon Meat": 0.9,
+        "Koka Noodles": 1
+    },
+    "Bubble Tea": {
+        "Milk Tea": 2.5,
+        "Green Milk Tea": 2.7,
+        "Jasmine Green Tea": 2.5,
+        "Oolong Milk Tea": 2.5,
+        "Brown Sugar Milk Tea": 3,
+        "Honey Milk Tea": 2.7,
+        "Strawberry Milk Tea": 3,
+    },
+    "Takoyaki": {
+        "Original": 3.8,
+        "Cheese Takoyaki": 4,
+        "Ham And Cheese Takoyaki": 4.5,
+        "Octopus Takoyaki": 4.5
+    },
+    "Snack": {
+        "Muah Chee": 2.5,
+        "Sandwich": 2,
+        "Sausage": 1,
+        "Fries": 2
+    },
+    "Waffle": {
+        "Plain Waffle": 2,
+        "Strawberry Waffle": 2.5,
+        "Chocolate Waffle": 2.5,
+        "Honey Butter Waffle": 2.8,
+    }
+    
+}
